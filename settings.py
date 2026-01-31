b52907a403d8b9eadcead68a2cc2a5bcd745e1f5
PaGarrigaCo <paugarrigacomas@gmail.com>
2026-01-31 09:54:35 +0100

    millores
    De moment deixo el projecte aquí, continuarà més endavant

--- settings.py
+++ settings.py
@@ -1,6 +1,19 @@
 class Settings:
     def __init__(self):
+        #CONFIGURACIÓ PANTALLA
         self.screen_width = 1200
         self.screen_height = 800
-        self.ship_speed = 0.5
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
+        #CONFIGURACIÓ NAU
+        self.ship_speed = 1
+        self.ship_limit = 3
+        #CONFIGURACIÓ LASER
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color=(255,0,0)
+        self.bullets_allowed = 3
+        #CONFIGURACIÓ ALIENS
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
